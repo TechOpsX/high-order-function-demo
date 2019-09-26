04aefa104694d367a3c4feef2bf4251e2eda7693
Weiping <gan.weiping@thoughtworks.com>
2019-09-26 18:50:15 +0800

    add closure test

--- func/closure.py
+++ func/closure.py
@@ -20,8 +20,27 @@ def count_beta():
     return fs
 
 
+# 练习 利用闭包返回一个计数器函数，每次调用它返回递增整数：
+def create_counter():
+    i = 0
+
+    def counter():
+        nonlocal i
+        i = i + 1
+        return i
+
+    return counter
+
+
 if __name__ == '__main__':
     f1, f2, f3 = count()
     print(f1())
     print(f2())
     print(f3())
+    counterA = create_counter()
+    print(counterA(), counterA(), counterA(), counterA(), counterA())  # 1 2 3 4 5
+    counterB = create_counter()
+    if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+        print('测试通过!')
+    else:
+        print('测试失败!')
